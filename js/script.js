e3ebe8b1ba90cbd00a8fc212fe9c7d417ad13aa6
mac <mac@MacBook-Pro-mac.local>
2019-06-20 19:21:49 +0300

    Add stub for 404 img

--- js/script.js
+++ js/script.js
@@ -14,17 +14,29 @@ function apiSearch(event) {
         return value.json();
     }).then(function (output) {
         let inner = '';
+        let errorUrl = '';
+        let stub = '';
+
         output.results.forEach(function (item, i, array) {
             let nameItem = item.name || item.title;
-            
+            if(item.poster_path != undefined){
+                stub = urlPoster + item.poster_path;
+            }else{
+                stub = './error.png';
+            }
             inner += `<div class="col-12 col-md-4 col-xl-3 item" >
-            <img src="${urlPoster + item.poster_path}" alt="${nameItem}">
+            <img src="${stub}" alt="${nameItem}">
             ${nameItem}</div>`;
+            console.log(item.poster_path);
         });
         movie.innerHTML = inner;
-    }).catch(function (err) {
+    })
+    .catch(function (err) {
         movie.innerHTML = 'Что то не так!';
+
         console.log('error' + err.status);
+        console.log(value.status);
     });
 }
+
 searchForm.addEventListener('submit', apiSearch);
