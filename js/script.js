db35becdfda1e14bc54a1653865fdbacd9235905
mac <mac@MacBook-Pro-mac.local>
2019-06-21 16:31:33 +0300

    Add onclick funck for image + fix attr data-type

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function apiSearch(event) {
     })
     .catch(function (err) {
         movie.innerHTML = 'Что то не так!';
-
+        console.error(err||err.status);
     });
 }
 
@@ -63,12 +63,39 @@ function addEventMedia (){ // Функция клика по картинке
 }
 
 function showFullInfo(){ // переход на страницу фильма
-    console.log(this);
+    let url = '';
+    if(this.dataset.type === 'movie'){
+        url = `https://api.themoviedb.org/3/movie/${this.dataset.id}?api_key=797193b6dc6621834944c35a7b47b47f&language=ru`;
+    }else if(this.dataset.type === 'tv'){
+        url = `https://api.themoviedb.org/3/tv/${this.dataset.id}?api_key=797193b6dc6621834944c35a7b47b47f&language=ru`;
+    } else {
+         movie.innerHTML = '<h2 class=" col-12 text-center text-danger">Произошла Ошибка</h2>';
+    }
+
+    fetch(url).then(function (value) {
+        if(value.status !== 200){
+            return Promise.reject(new Error(value.status));
+        }
+        return value.json();
+    }).then(function (output) {// !!!
+        console.log(output);
+       movie.innerHTML = `<h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
+       <div class="col-4">
+       <img src = '${output.poster_path}'>
+       </div>
+       <div class="col-8"></div>`; 
+    })
+    .catch(function (reason) {
+        movie.innerHTML = 'Что то не так!';
+        console.error(reason||reason.status);
+
+    });
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{ // Страница "Популярое за неделю"
     
-    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=797193b6dc6621834944c35a7b47b47f&language=ru').then(function (value) {
+    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=797193b6dc6621834944c35a7b47b47f&language=ru')
+    .then(function (value) {
         if(value.status !==200){
             return Promise.reject();
         }
@@ -76,23 +103,23 @@ document.addEventListener('DOMContentLoaded', ()=>{ // Страница "Поп�
     }).then(function (output) {
         let inner = `<h2 class="col-12 text-center text-info">Пополярные за неделю</h2>`;
        if(output.results.length === 0){
-           inner = `<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h2>` // 2
+        inner = `<h2 class="col-12 text-center text-info">По вашему запросу ничего не найденно</h2>`;
        }
         let errorUrl = '';
         let stub = '';
 
         output.results.forEach(function (item) {
             let nameItem = item.name || item.title;
-            if(item.poster_path != undefined){ // подстановна error.png на фильм без картинки
+            let mediaType = item.title ? 'movie' : 'tv';
+            if(item.poster_path){ // подстановна error.png на фильм без картинки
                 stub = urlPoster + item.poster_path;
             }else{
                 stub = './error.png';
             }
-            dataInfo = `data-id="${item.id}" data-type = "${item.media_type}"`; // !!!
+            dataInfo = `data-id="${item.id}" data-type = "${mediaType}"`; // !!!
             inner += `<div class="col-12 col-md-4 col-xl-3 item" >
             <img src="${stub}" alt="${nameItem} "${dataInfo}> 
             ${nameItem}</div>`;
-            console.log(item.poster_path);
         });
         movie.innerHTML = inner;
         addEventMedia ();
